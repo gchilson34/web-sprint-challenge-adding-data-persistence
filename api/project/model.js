d8e6e5b0018311ed8fdcd4a235298a2c5089e312
Gabe Chilson <g.chilson34@gmail.com>
2021-10-24 13:39:46 -0600

    finished MVP tasks

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,35 @@
-// build your `Project` model here
+const db = require("../../data/dbConfig")
+
+async function getAll() {
+      const projects = await db('projects')
+      return projects.map(p => {
+            return {
+                  ...p,
+                  project_completed : p.project_completed ? true : false
+            }
+      })
+}
+
+async function getById(project_id) { 
+      const project = await db('projects')
+            .where({ project_id })
+            .first()
+      
+            return {
+                  ...project,
+                  project_completed : project.project_completed ? true : false
+            }
+}
+
+async function add(newProject) {
+      const { id } = await db('projects')
+            .insert(newProject)
+
+      return getById(id)
+}
+
+module.exports = {
+      getAll,
+      getById,
+      add
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,24 @@
-// build your `/api/projects` router here
+const express = require('express')
+
+const Project = require('./model')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+      Project.getAll()
+            .then(projects => {
+                  res.json(projects)
+            })
+            .catch(next)
+})
+
+router.post('/', async (req, res, next) => {
+      try {
+            const addProject = await Project.add(req.body)
+            res.status(201).json(addProject)
+      }catch(e) {
+            next(e)
+      }
+})
+
+module.exports = router
